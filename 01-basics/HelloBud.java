328a90fbfee7559cc0f1d726e48eb168fcef9433
Anish <anishbheron@gmail.com>
2026-08-08 17:13:02 +0000

    Add HelloBud and Variable classes with basic output

---
+++ 01-basics/HelloBud.java
@@ -0,0 +1,5 @@
+public class HelloBud {
+    public static void main(String[] args) {
+        System.out.println("Hello, Buddy!");
+    }
+}
---
+++ 01-basics/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        int luckyNumber = 3;
+        String hello = "Hello, World!";
+        boolean isAnishGreat = true;
+        double pi = 3.14;
+        long population = 7800000000L;
+        float gravity = 9.81f;
+
+        System.out.println("Integer: " + luckyNumber);
+        System.out.println("String: " + hello);
+        System.out.println("Boolean: " + isAnishGreat);
+        System.out.println("Double: " + pi);
+        System.out.println("Long: " + population);
+        System.out.println("Float: " + gravity);
+    }
+}
