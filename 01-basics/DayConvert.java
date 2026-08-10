3529fa24505a065f901ed77d6e59c1c8d63a8151
Anish <anishbheron@gmail.com>
2026-08-10 09:35:10 +0000

    Refactor DayConvert class name and structure for consistency

--- 01-basics/DayConvert.java
+++ 01-basics/DayConvert.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
-class DayConvert1 
-{
-	
-	public static void main (String[] args) 
-	{
+class DayConvert{
+    public static void main (String[] args) {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Enter number of days : ");
 			int days = Integer.valueOf(scanner.nextLine());
